8b343aa8d653c9521278fa6f616c48e031215c25
andrea <lucaandrea264@gmail.com>
2025-10-03 23:55:16 +0200

    aggiorna per il deploy

--- server/db.ts
+++ server/db.ts
@@ -1,15 +1,21 @@
+import dotenv from 'dotenv';
 import { Pool, neonConfig } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
 import ws from "ws";
 import * as schema from "@shared/schema";
 
+// Se esiste .env (locale), carica le variabili
+dotenv.config();
+
 neonConfig.webSocketConstructor = ws;
 
+// Controllo DATABASE_URL
 if (!process.env.DATABASE_URL) {
   throw new Error(
     "DATABASE_URL must be set. Did you forget to provision a database?",
   );
 }
 
+// Pool e Drizzle
 export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 export const db = drizzle(pool, { schema });
