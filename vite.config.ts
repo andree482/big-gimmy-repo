7605ce19fa1167517cb3d35843e4a255bbd63277
andrea <lucaandrea264@gmail.com>
2025-10-12 11:25:32 +0200

    elimina log browser

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,9 @@ export default defineConfig({
       input: path.resolve(__dirname, 'client/index.html')
     }
   },
+    server: {
+    hmr: false, // disabilita HMR
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'client/src'),
